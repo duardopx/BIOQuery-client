3c3e902b176d6ab38ffdd7bf0b931632eb15c0de
duardopx <duardopx@protonmail.com>
2020-11-10 09:09:35 -0300

    Initializing wikipage

---
+++ src/pages/Wiki.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/pages/Wiki.css';
+
+function Wiki() {
+    return (
+        <div id="page-wiki">
+            <div className="content-wrapper">
+                <h1>test</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Wiki;
